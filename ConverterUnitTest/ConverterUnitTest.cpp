50ca52373ee67a63f45dbae1e0a1c152a2a9438b
GulinAlexey <gulin-2001@mail.ru>
2023-03-13 03:36:14 +0700

    Добавление проекта ConverterUnitTest для написания тестов

---
+++ ConverterUnitTest/ConverterUnitTest.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ConverterUnitTest
+{
+	TEST_CLASS(ConverterUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
