ae823647fc5bc7705e623160b3d99f18fcc2682e
GulinAlexey <gulin-2001@mail.ru>
2023-03-13 04:14:48 +0700

    Доб. тесты перевода 1 дюйма в метр и обратно

--- ConverterUnitTest/ConverterUnitTest.cpp
+++ ConverterUnitTest/ConverterUnitTest.cpp
@@ -17,6 +17,18 @@ namespace ConverterUnitTest
 			ConvertAmericanToSi* conv = new ConvertAmericanToSi();
 			Assert::IsNotNull(conv);
 		}
+		TEST_METHOD(Inch1ToMetreTest)
+		{
+			double res = ConvertAmericanToSi::inchToMetre(1, true);
+
+			Assert::AreEqual(0.025, res);
+		}
+		TEST_METHOD(Metre1ToInchTest)
+		{
+			double res = ConvertAmericanToSi::inchToMetre(1, false);
+
+			Assert::AreEqual(39.37, res);
+		}
 	};
 
 	TEST_CLASS(ConvertRussianToSiTest)
